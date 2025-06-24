34e761c2282dfb166d3e13ff13a4d08815862b79
Andemand111 <s214630@dtu.dk>
2025-06-24 22:06:27 +0200

    maybe a solution?

--- pi/start_wifi_sniffer.sh
+++ pi/start_wifi_sniffer.sh
@@ -10,24 +10,20 @@ echo "$(date): Starting Wi-Fi sniffer setup..." >> "$LOG_FILE"
 PROJECT_DIR="/home/$PYTHON_DEVICE_NAME/Roskilde-Projekt/"
 echo "$(date): Project directory set to $PROJECT_DIR." >> "$LOG_FILE"
 
-# check and try 5 times with 5 seconds interval if the wlan1 interface is up
-for i in {1..5}; do
-    ip link set wlan1 down >> "$LOG_FILE" 2>&1
-    iw dev wlan1 set type monitor >> "$LOG_FILE" 2>&1
-    ip link set wlan1 up >> "$LOG_FILE" 2>&1
-    if iw dev wlan1 info | grep -q "type monitor"; then
-        echo "$(date): wlan1 is successfully set to monitor mode." >> "$LOG_FILE"
-        break
-    else
-        echo "$(date): Attempt $i: wlan1 is not in monitor mode, retrying in 5 seconds..." >> "$LOG_FILE"
-        sleep 5
-    fi
-done
+# try to put wlan1 into monitor mode
+ip link set wlan1 down >> "$LOG_FILE" 2>&1
+iw dev wlan1 set type monitor >> "$LOG_FILE" 2>&1
+ip link set wlan1 up >> "$LOG_FILE" 2>&1
 
+# Check if wlan1 is in monitor mode
+# if not, exit with an error
+# the .service file will make sure that this script is run again if it fails
 if ! iw dev wlan1 info | grep -q "type monitor"; then
     echo "$(date): Failed to set wlan1 to monitor mode after 5 attempts." >> "$LOG_FILE"
     exit 1
 fi
+
+echo "$(date): Successfully set wlan1 to monitor mode." >> "$LOG_FILE"
 
 cd "$PROJECT_DIR" >> "$LOG_FILE" 2>&1
 echo "$(date): Changed directory to $PROJECT_DIR." >> "$LOG_FILE"
@@ -38,4 +34,6 @@ echo "$(date): Found virtual environment Python: $VENV_PYTHON" >> "$LOG_FILE"
 "$VENV_PYTHON" raspberry.py --device_name="$PYTHON_DEVICE_NAME" >> "$LOG_FILE" 2>&1
 echo "$(date): Python script terminated." >> "$LOG_FILE"
 
-exit $?
+# if the script fails, exit with an error and restart the service
+echo "$(date): The python script has exited for some reason (see log). Restarting the service..." >> "$LOG_FILE"
+exit 1
