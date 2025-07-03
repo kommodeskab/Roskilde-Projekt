a33de9db07991fae07b1e3c9c101af340bf6f9ff
Andemand111 <s214630@dtu.dk>
2025-07-03 15:10:46 +0200

    fix for device 1

--- raspberry.py
+++ raspberry.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from sniff import sniff_packets
 import sys
 
+DUMMY_TIME = 15
 SCAN_DURATION = 300
 MAX_LENGTH = 49_000
 
@@ -19,7 +20,6 @@ def dummy_data() -> dict[str, int]:
 def get_crowd_data(scan_duration : int) -> dict[str, int]:
     interface = 'alfa' 
     return sniff_packets(interface, scan_duration) 
-    # return dummy_data()
 
 def split_dict_by_max_length(input_dict : dict, max_length : int) -> list[dict]:
     result = []
@@ -46,57 +46,50 @@ def main():
     args = parser.parse_args()
     device_name : str = args.device_name
     
-    # start by sniffing for 30 seconds
-    # if no devies are picked up, we exit the script
-    dummy_data = get_crowd_data(30)
-    if len(dummy_data) == 0:
-        print("No data found during dummy trial, restarting...", flush=True)
+    print(f"Starting sniffing on {device_name} for {SCAN_DURATION} seconds...", flush=True)
+            
+    try:
+        crowd_data = get_crowd_data(SCAN_DURATION)
+                
+    except OSError as e:
+        print(f"Error sniffing packets: {e}", flush=True)
         sys.exit(1)
         
-    print("Dummy trial successful, starting main loop...", flush=True)
+    except Exception as e:
+        print(f"Unexpected error: {e}", flush=True)
+        sys.exit(1)
         
-    while True:
-        try:
-            crowd_data = get_crowd_data(SCAN_DURATION)
-                    
-        except OSError as e:
-            print(f"Error sniffing packets: {e}", flush=True)
-            sys.exit(1)
-            
-        except Exception as e:
-            print(f"Unexpected error: {e}", flush=True)
-            sys.exit(1)
-            
-        if len(crowd_data) == 0:
-            # if no data is found, it is most likely because the interface is not in monitor mode
-            # exiting the script allows the raspberry to set the interface to monitor mode again
-            print("No data found, trying a restart...", flush=True)
-            sys.exit(1)
-        
-        # format the timestamp as 'YYYY-MM-DD HH:MM'
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M')
-        
-        # this is a list of strings to be logged
-        crowd_data = split_dict_by_max_length(crowd_data, MAX_LENGTH)
-        
-        data = [
-            {
-            "device_name": device_name,
-            "timestamp": timestamp,
-            "crowd_data": str(d),
-            } 
-            for d in crowd_data
-            ]
-        
-        try:
-            write_data(data)
-        except Exception as e:
-            print(f"Error writing data: {e}", flush=True)
-            sys.exit(1)
-        
-        print(f"Data written at {timestamp} with number of people (per write):", flush=True)
-        for d in crowd_data:
-            print(f"  {len(d)}", flush=True)
+    if len(crowd_data) == 0:
+        # if no data is found, it is most likely because the interface is not in monitor mode
+        # exiting the script allows the raspberry to set the interface to monitor mode again
+        print("No data found, trying a restart...", flush=True)
+        sys.exit(1)
+    
+    # format the timestamp as 'YYYY-MM-DD HH:MM'
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M')
+    
+    # this is a list of strings to be logged
+    # due to google sheets limitations, only 50,000 characters can be written at once
+    crowd_data = split_dict_by_max_length(crowd_data, MAX_LENGTH)
+    
+    data = [
+        {
+        "device_name": device_name,
+        "timestamp": timestamp,
+        "crowd_data": str(d),
+        } 
+        for d in crowd_data
+        ]
+    
+    try:
+        write_data(data)
+    except Exception as e:
+        print(f"Error writing data: {e}", flush=True)
+        sys.exit(1)
+    
+    print(f"Data written at {timestamp} with number of people (per write):", flush=True)
+    for d in crowd_data:
+        print(f"  {len(d)}", flush=True)
         
 if __name__ == "__main__":
     main()
